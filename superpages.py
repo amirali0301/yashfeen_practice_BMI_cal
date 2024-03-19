fc05e947cde9f355d8723abac47f356182326752
amirali0301 <111804468+amirali0301@users.noreply.github.com>
2024-03-19 15:50:43 +0500

    Update superpages.py

--- superpages.py
+++ superpages.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title('Simple Greeting App')
+    st.title('love app')
     
     # Text input for user's name
-    name = st.text_input('Enter your name:')
+    name = st.text_input('Enter your thark:')
     
     # Check if name is provided
     if name:
         # Display greeting message
-        st.write(f'Hello, {name}! Welcome to Streamlit.')
+        st.write(f'chor dy, {name} ki jan ukhar ly kch zindgi me.')
 
 if __name__ == "__main__":
     main()
